dd3f7ae653de24c589b0a4fcb8af9163398197c1
Joe Bingham <91940376+88joe@users.noreply.github.com>
2022-05-21 21:38:29 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute(){
+
+        var amount = Number(document.getElementById("amount").value);
+        var ir = Number(document.getElementById("rate").value);
+        var select = document.getElementById("years");
+        var years = Number(select.options[select.selectedIndex].value);
+        var thisYear = new Date().getFullYear();
+
+    if(amount <= 0 || "" == document.getElementById("amount").value){
+        alert("Enter a positive Number")
+        document.getElementById("amount").focus();
+        }else{
+
+    document.getElementById("damount").innerHTML = amount;
+    document.getElementById("drate").innerHTML = rate;
+    document.getElementById("dyears").innerHTML = years;
+    document.getElementById("dresult").innerHTML = result;
+    document.getElementById("resText").style.display = "block";
+        }
 }
+
         
